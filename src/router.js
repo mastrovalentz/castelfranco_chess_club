edc85322f2c8eca503ff1c557c3a63aa459cf1a5
Alessandro Valente <alevalentz@gmail.com>
2023-07-23 18:03:17 +0200

    CCC-01 Basic routing works

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
+import { ROUTES } from './routes.js'
+
 const primaryNav = document.getElementById('primary-navigation')
 primaryNav.addEventListener('click', (event) => {
-    const {target} = event
-    if (!target.matches("nav a")) {
+    const { target } = event
+    if (!target.matches('nav a')) {
         return null
     }
     event.preventDefault()
@@ -14,5 +16,17 @@ const urlRoute = (target) => {
 }
 
 const urlLocationHandler = async () => {
+    let location = window.location.pathname
+    if (location.length === 0) {
+        location = '/'
+    }
 
+    // TODO: improve on this with default
+    const route = ROUTES[location] || ROUTES[404]
+    // TODO: improve on this with reoute class ??
+    document.getElementById('page-content').innerHTML = await fetch(
+      route.template
+    ).then((template) => template.text())
 }
+
+urlLocationHandler()
--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,22 @@
 export const ROUTES = {
     404: {
-        template: "/pages/404.html",
-        title: "",
-        description: ""
+        template: 'src/pages/404.html',
+        title: '',
+        description: ''
     },
-    "/": {
-        template: "/pages/home.html",
-        title: "",
-        description: ""
+    '/': {
+        template: 'src/pages/home.html',
+        title: '',
+        description: ''
     },
-    "/about": {
-        template: "/pages/about.html",
-        title: "",
-        description: ""
+    '/about': {
+        template: 'src/pages/about.html',
+        title: '',
+        description: ''
     },
-    "/contacts": {
-        template: "/pages/contacts.html",
-        title: "",
-        description: ""
+    '/contacts': {
+        template: 'src/pages/contacts.html',
+        title: '',
+        description: ''
     }
 }
