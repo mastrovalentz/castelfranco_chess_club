92c6abb24037c702c89b63b7c75e19deb0a8f10c
Alessandro Valente <alevalentz@gmail.com>
2023-07-23 17:49:53 +0200

    CCC-01 Create pages and routes

--- src/router.js
+++ src/router.js
@@ -1,1 +1,18 @@
-const primaryNav = document.getElementsByClassName("primary-navigation");
+const primaryNav = document.getElementById('primary-navigation')
+primaryNav.addEventListener('click', (event) => {
+    const {target} = event
+    if (!target.matches("nav a")) {
+        return null
+    }
+    event.preventDefault()
+    urlRoute(target)
+})
+
+const urlRoute = (target) => {
+    window.history.pushState(null, null, target.href)
+    urlLocationHandler()
+}
+
+const urlLocationHandler = async () => {
+
+}
---
+++ src/routes.js
@@ -0,0 +1,22 @@
+export const ROUTES = {
+    404: {
+        template: "/pages/404.html",
+        title: "",
+        description: ""
+    },
+    "/": {
+        template: "/pages/home.html",
+        title: "",
+        description: ""
+    },
+    "/about": {
+        template: "/pages/about.html",
+        title: "",
+        description: ""
+    },
+    "/contacts": {
+        template: "/pages/contacts.html",
+        title: "",
+        description: ""
+    }
+}
