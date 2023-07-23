ebdd4b2892b04e2c1b4bccc7d3a1d71dd4b44387
Alessandro Valente <alevalentz@gmail.com>
2023-07-23 18:07:29 +0200

    CCC-01 Lint

--- src/router.js
+++ src/router.js
@@ -2,31 +2,36 @@ import { ROUTES } from './routes.js'
 
 const primaryNav = document.getElementById('primary-navigation')
 primaryNav.addEventListener('click', (event) => {
-    const { target } = event
-    if (!target.matches('nav a')) {
-        return null
-    }
-    event.preventDefault()
-    urlRoute(target)
+  const { target } = event
+  if (!target.matches('nav a')) {
+    return null
+  }
+  event.preventDefault()
+  urlRoute(target)
 })
 
 const urlRoute = (target) => {
-    window.history.pushState(null, null, target.href)
-    urlLocationHandler()
+  window.history.pushState(null, null, target.href)
+  urlLocationHandler()
 }
 
+// TODO handle fetch error with 404 page
 const urlLocationHandler = async () => {
-    let location = window.location.pathname
-    if (location.length === 0) {
-        location = '/'
-    }
+  let location = window.location.pathname
+  if (location.length === 0) {
+    location = '/'
+  }
 
-    // TODO: improve on this with default
-    const route = ROUTES[location] || ROUTES[404]
-    // TODO: improve on this with reoute class ??
-    document.getElementById('page-content').innerHTML = await fetch(
-      route.template
-    ).then((template) => template.text())
+  // TODO: improve on this with default
+  const route = ROUTES[location] || ROUTES[404]
+  // TODO: improve on this with reoute class ??
+  document.getElementById('page-content').innerHTML = await fetch(
+    route.template
+  ).then((template) => template.text())
 }
 
+window.onpopstate = urlLocationHandler
+window.route = urlRoute
+
+// TODO create wrapper to persist promise and handle errors
 urlLocationHandler()
